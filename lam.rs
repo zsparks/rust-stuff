80752b66905af81ae1444d6cd7ff3e430c6b64fa
Zachary Austin Sparks <zasparks@cs.indiana.edu>
2013-01-09 14:45:42 -0500

    added evaluation

--- lam.rs
+++ lam.rs
@@ -36,6 +36,21 @@ impl Tp: ToStr {
 
 }
 
+impl Exp: ToStr {
+
+  pure fn to_str() -> ~str {
+    match self {
+      Triv => ~"Triv"
+    , App (@e1, @e2) =>
+        str::concat ([e1.to_str(), ~" ", e2.to_str()])
+    , Lam (@t, @body) => 
+        str::concat ([~"Lambda ", t.to_str(), ~" (", body.to_str(), ~")"])
+    , Var (i) => i.to_str()
+    }
+  }
+
+}
+
 enum Exp {
   Triv
 , Lam (@Tp, @Exp)
@@ -77,11 +92,69 @@ pure fn typecheck (ctx: List<@Tp>, exp: @Exp) -> Option<@Tp> {
   }
 }
 
+pure fn value (e: @Exp) -> bool {
+  match *e {
+    Triv => true
+  , Lam (*) => true
+  , _ => false
+  }
+}
+
+pure fn subst_i (e: @Exp, ehole: @Exp, i: int) -> @Exp {
+  match *ehole {
+    Var (j) if j == i => e
+  , Var (j) => @ Var (j - 1)
+  , App (e1, e2) => @ App (subst_i (e, e1, i), subst_i (e, e2, i))
+  , Lam (t, body) => @ Lam (t, subst_i (e, body, i + 1))
+  , Triv => ehole
+  }
+}
+
+pure fn subst (e: @Exp, ehole: @Exp) -> @Exp {
+  subst_i (e, ehole, 0)
+}
+
+pure fn step (e: @Exp) -> Option<@Exp> {
+  match *e {
+    App (e1 @ @Lam (_, body), e2) =>
+      if value (e2) {
+        Some (subst (e2, body))
+      }
+      else {
+        do chain (step (e2))
+          |e2out| { Some (@App (e1, e2out)) }
+      }
+  , _ => None
+  }
+}
+
+pure fn eval (e: @Exp) -> Option<@Exp> {
+  if value (e) {
+    Some (e)
+  }
+  else {
+    chain (step (e), eval)
+  }
+
+}
+
 fn typecheck_and_print (e: @Exp) {
   do iter (@ typecheck (Nil, e))
     |out| { io::println ((**out).to_str()); };
 }
 
+fn eval_and_print (e: @Exp) {
+  match (eval (e)) {
+    None => io::println ("Failure.")
+  , Some (v) => io::println (v.to_str())
+  }
+}
+
+
 fn main () {
-  typecheck_and_print (@Lam (@Unit, @Var (0)));
+  let id_exp : @Exp = @Lam (@Unit, @Var (0));
+  let id_app_triv : @Exp = @App (id_exp, @Triv);
+  typecheck_and_print (id_exp);
+  typecheck_and_print (id_app_triv);
+  eval_and_print (id_app_triv);
 }
